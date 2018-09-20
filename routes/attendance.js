ff9104a3f3c6ccfd5661f32a517f706179a47022
Snehal Kale <snehal.kale@eternussolutions.com>
2018-09-20 14:10:11 +0530

    added useeType field

--- routes/attendance.js
+++ routes/attendance.js
@@ -6,7 +6,6 @@ const _ = require("lodash");
 router.get("/", async (req, res) => {
   try {
     const result = await Attendance.find()
-      .populate("attendee", "firstName lastName")
       .populate("event", "eventName")
       .populate("session", "sessionName");
     res.send(result);
@@ -21,7 +20,14 @@ router.post("/", async (req, res) => {
     if (error) return res.status(404).send(error.details[0].message);
 
     const attendance = new Attendance(
-      _.pick(req.body, ["attendee", "event", "session", "scannedBy", "time"])
+      _.pick(req.body, [
+        "userId",
+        "userType",
+        "event",
+        "session",
+        "scannedBy",
+        "time"
+      ])
     );
     const result = await attendance.save();
     res.send(result);
@@ -35,6 +41,24 @@ router.delete("/:id", async (req, res) => {
     const result = await Attendance.findByIdAndRemove(req.params.id);
     if (!result) return res.status(404).send("not found");
     res.send(result);
+  } catch (error) {
+    res.send(error.message);
+  }
+});
+
+//get by eventId, sessionId
+router.get("/bySessionEvent/:eventId/:sessionId", async (req, res) => {
+  try {
+    const attendance = await Attendance.find()
+      .where("event")
+      .equals(req.params.eventId)
+      .where("session")
+      .equals(req.params.sessionId);
+    if (!attendance)
+      return res
+        .status(404)
+        .send("The attendance with the given session ID was not found.");
+    res.send(attendance);
   } catch (error) {
     res.send(error.message);
   }
